fe0151f1e9b5e7dd4029cbf0fa2aa0408aee5648
Mike Duin <michael.s.duin@gmail.com>
2017-02-07 14:06:31 -0800

    add results data to table

--- routes/results.js
+++ routes/results.js
@@ -13,7 +13,6 @@ function Users() {
 router.get('/', function(req, res, next){
   var resObj = {};
   Wagers().select('username', 'event').sum('net_total').groupBy('username', 'event').orderBy('event').then(function(results){
-    console.log(results);
     for (var i=0; i<results.length; i++) {
       if (resObj[results[i].username] === undefined) {
         resObj[results[i].username] = {
@@ -31,15 +30,12 @@ router.get('/', function(req, res, next){
         resObj[results[i].username]["TOTAL"] += parseInt(results[i].sum);
       }
     };
-    console.log(resObj);
     var sortedRes = [];
     for (var key in resObj) {
-      sortedRes.push({key: key, rising_stars: resObj[key]["Rising Stars Game"], skills: resObj[key]["Skills Challenge"], three_pt: resObj[key]["Three-Point Contest"], dunk: resObj[key]["Dunk Contest"], all_star: resObj[key]["All-Star Game"], total: resObj[key]["TOTAL"]})
+      sortedRes.push({username: key, rising_stars: resObj[key]["Rising Stars Game"], skills: resObj[key]["Skills Challenge"], three_pt: resObj[key]["Three-Point Contest"], dunk: resObj[key]["Dunk Contest"], all_star: resObj[key]["All-Star Game"], total: resObj[key]["TOTAL"]})
     };
     sortedRes.sort(function(x, y){return y.total - x.total});
-    console.log('sortedRes is ', sortedRes);
-
-    res.render('results');
+    res.render('results', {results: sortedRes});
   })
 })
 
