e0164e3b75413fa71518a7b848cb4af190e2ecdb
Mike Duin <michael.s.duin@gmail.com>
2017-02-06 20:30:48 -0800

    construct wager odds access

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,7 @@
 console.log('sanity check')
 
 $(document).ready(function(){
+
   $('#slideshow img:gt(0)').hide();
   setInterval(function(){
     $('#slideshow :first-child').fadeOut(5000)
@@ -8,11 +9,9 @@ $(document).ready(function(){
     .end().appendTo('#slideshow');
   }, 5000)
 
-  // $('#background-rotate img:gt(0)').hide();
-  // setInterval(function(){
-  //   $('#background-rotate :first-child').fadeOut(10000)
-  //   .next('img').fadeIn(10000)
-  //   .end().appendTo('#background-rotate');
-  // }, 10000)
+  $('.risk-input').on("change", function(){
+    console.log($(this)[0].value);
+    console.log('odds are ', $(this).parent().prev()[0].innerHTML);
+  });
 
 });
