4f10a26edbd40e69b67cc3dd0b4b0e0d3b6e75b5
Mike Duin <michael.s.duin@gmail.com>
2017-02-16 13:49:53 -0800

    add new users migration

---
+++ migrations/20170216134926_create_users.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(t){
+    t.increments();
+    t.string('email');
+    t.string('username');
+    t.string('first_name');
+    t.string('last_name');
+    t.string('fb_profile');
+    t.string('facebookid');
+    t.string('photo');
+    t.boolean('superuser');
+    t.float('balance').defaultTo(1000);
+    t.float('asg').defaultTo(0);
+    t.float('dunk').defaultTo(0);
+    t.float('threept').defaultTo(0);
+    t.float('skills').defaultTo(0);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
