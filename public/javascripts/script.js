ef6df5155130c3ab4753d213a08c01199f884754
Mike Duin <michael.s.duin@gmail.com>
2017-02-17 18:26:25 -0800

    fix undefined net total

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -256,6 +256,8 @@ $(document).ready(function(){
             netTotal = '$' + wagers[i].net_total
           } else if (wagers[i].net_total < 0) {
             netTotal = '-$' + wagers[i].net_total
+          } else {
+            netTotal = ''
           };
           if (moment(wagers[i].start_time).subtract(8, 'hours').isBefore(moment())) {
             table.append('<tr><td>' + wagers[i].username + '</td><td>' + wagers[i].event + '</td><td>' + wagers[i].type + '</td><td>' + wagers[i].wager + '</td><td>' + odds + '</td><td> $' + wagers[i].risk + '</td><td> $' + wagers[i].to_win + '</td><td>' + netTotal + '</td></tr>');
